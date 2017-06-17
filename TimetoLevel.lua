2f144393582843b85cf45536fe5f8880a5d3f428
Megan Davis <davisma24@vcu.edu>
2017-06-16 23:11:46 -0400

    Changed algorithm for calculating nums and time; fixed addon.name issue

--- TimetoLevel.lua
+++ TimetoLevel.lua
@@ -4,24 +4,24 @@ TimetoLevel = {}
 ------------------------------------------------------------------------------------------------
 TimetoLevel.name = "TimetoLevel"
 TimetoLevel.version = 1
-TimetoLevel.XP = 0
-TimetoLevel.levelXP = 0
-TimetoLevel.remainingXP = 0
-TimetoLevel.initialXP = 0
---TimetoLevel.monsters = 0
---TimetoLevel.avgMonsterXP = 0
+TimetoLevel.XP = GetUnitXP('player')
+TimetoLevel.levelXP = GetNumExperiencePointsInLevel(GetUnitLevel('player')) 
+TimetoLevel.remainingXP = TimetoLevel.levelXP - TimetoLevel.XP
+TimetoLevel.initialXP = GetUnitXP('player')
+
+
+
+TimetoLevel.avgMonsterXP = 0
+TimetoLevel.avgDiscoverXP = 0
+TimetoLevel.avgQuestXP = 0
+TimetoLevel.avgXP = 1
 
 --Initalize Function
 function TimetoLevel.Initalize(eventCode, addOnName)
 	--TimetoLevelCounter:SetText(string.format(TimetoLevel.name)
-	if ( addOnName ~= "TimetoLevel") then
+	if ( addOnName ~= TimetoLevel.name) then
 		return
 	end
-
-	TimetoLevel.XP = GetUnitXP('player')
-	TimetoLevel.levelXP = GetNumExperiencePointsInLevel(GetUnitLevel('player')) 
-	TimetoLevel.remainingXP = TimetoLevel.levelXP - TimetoLevel.XP
-	TimetoLevel.initialXP = GetUnitXP('player')
 
 	EVENT_MANAGER:UnregisterForEvent(TimetoLevel.name, EVENT_ADD_ON_LOADED)
 end
@@ -31,32 +31,54 @@ function TimetoLevel.Update(eventCode, unitTag, currentExp, maxExp, reason)
        d("You gained " .. XPgain .. " experience.")
        TimetoLevel.XP = currentExp
 	   TimetoLevel.remainingXP = TimetoLevel.levelXP - TimetoLevel.XP
+	   
 	   if(reason == 0) then
-			d("Monster kill")
-			TimetoLevelMonstersNum:SetText(zo_round(TimetoLevel.remainingXP/XPgain))
+			--d("Monster kill and counter".. monsterCounter)		
+			TimetoLevel.avgMonsterXP = (.1 * XPgain) + (.9 * TimetoLevel.avgMonsterXP)
+			--TimetoLevelUIMonstersNum:SetText(zo_round(TimetoLevel.remainingXP/TimetoLevel.avgMonsterXP))
 	   elseif(reason == 1) then
 			d("Quest complete")
-			TimetoLevelQuestNum:SetText(zo_round(TimetoLevel.remainingXP/XPgain))
+			TimetoLevel.avgQuestXP = (.5 * XPgain) + (.5 * TimetoLevel.avgQuestXP)
+			--TimetoLevelUIQuestNum:SetText(zo_round(TimetoLevel.remainingXP/TimetoLevel.avgQuestXP))
 	   elseif(reason == 3) then
 			d("Discovered complete")
-			TimetoLevelDiscoverNum:SetText(zo_round(TimetoLevel.remainingXP/XPgain))
+			-- Discover XP is identical, so it doesn't need a formula
+			TimetoLevel.avgDiscoverXP = XPgain
+			--TimetoLevelUIDiscoverNum:SetText(zo_round(TimetoLevel.remainingXP/XPgain))
 	   else
 			d("Other XP event")
 	   end
-
+		TimetoLevel.UpdateOthers()
 end
 
 function TimetoLevel.AverageTime()
-	if (TimetoLevel.initialXP == GetUnitXP('player')) then
-		TimetoLevelTimeNum:SetText("Unknown")
-	else
-		local XPAMin = GetUnitXP('player') - TimetoLevel.initialXP
-		TimetoLevelTimeNum:SetText(zo_round(TimetoLevel.remainingXP / XPAMin))
-		TimetoLevel.initialXP = GetUnitXP('player')
-	end
-	
+	local XPAMin = GetUnitXP('player') - TimetoLevel.initialXP
+	TimetoLevel.avgXP = (.5 * XPAMin) + (.5 * TimetoLevel.avgXP)
+	TimetoLevelUITimeNum:SetText(zo_round(TimetoLevel.remainingXP/TimetoLevel.avgXP))
+	TimetoLevel.initialXP = GetUnitXP('player')
 	
 end
+
+function TimetoLevel.UpdateOthers()
+	TimetoLevelUIMonstersNum:SetText(zo_round(TimetoLevel.remainingXP/TimetoLevel.avgMonsterXP))
+	TimetoLevelUIQuestNum:SetText(zo_round(TimetoLevel.remainingXP/TimetoLevel.avgQuestXP))
+	TimetoLevelUIDiscoverNum:SetText(zo_round(TimetoLevel.remainingXP/TimetoLevel.avgDiscoverXP))
+end
+--[[
+function Average(theArray)
+	local totalXP = 0
+	local arraysize = #theArray
+	for i=1, arraysize do
+		totalXP = zo_round(totalXP + (theArray[i] * (.85)^(arraysize-i)))
+	end
+	d("Total XP :".. totalXP .. ".")
+	if(totalXP == 0) then
+		return 1
+	else
+		return (totalXP/arraysize)
+	end
+end--]]
+
 
 
 EVENT_MANAGER:RegisterForEvent(TimetoLevel.name, EVENT_ADD_ON_LOADED, TimetoLevel.Initalize)
